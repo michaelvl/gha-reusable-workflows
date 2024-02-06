d3513020c89520a18dee71ef908a638d4beceb25
michaelvl <mvl.gh@network42.dk>
2024-02-06 19:11:02 +0100

    Adjust vsa

--- scripts/vsa-poc.sh
+++ scripts/vsa-poc.sh
@@ -1,32 +1,22 @@
 #1 /bin/bash
 
 STATUS=$1
-SUBJECT=$2
-DIGEST=$3
-POLICY=$4
-POLICY_DIGEST=$5
+POLICY=$2
+POLICY_DIGEST=$3
 
-NOW=$(date --iso-8601=seconds)
+# This is also RFC3339 format - OPA understands this format
+NOW=$(date --iso-8601=seconds -u)
 
 cat <<EOF
 {
-  "_type": "https://in-toto.io/Statement/v1",
-  "subject": [{
-    "name": "$SUBJECT",
-    "digest": {"sha256": "$DIGEST"}
-  }],
-
-  "predicateType": "https://slsa.dev/verification_summary/v1",
-  "predicate": {
-    "verifier": {
-      "id": "$POLICY"
-    },
-    "timeVerified": "$NOW",
-    "policy": {
-      "uri": "$POLICY",
-      "digest": {"sha1": "$POLICY_DIGEST"}
-    },
-    "verificationResult": "$STATUS"
-  }
+  "verifier": {
+    "id": "$POLICY"
+  },
+  "verifiedAt": "$NOW",
+  "policy": {
+    "uri": "$POLICY",
+    "digest": {"sha1": "$POLICY_DIGEST"}
+  },
+  "verificationResult": "$STATUS"
 }
 EOF

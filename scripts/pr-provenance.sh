95d4a624e1c3a50ed277910f495f6186b09a4e5c
michaelvl <mvl.gh@network42.dk>
2024-02-08 18:12:03 +0100

    Correct chart builder

--- scripts/pr-provenance.sh
+++ scripts/pr-provenance.sh
@@ -7,13 +7,50 @@ SHA=$4
 
 #export PR=$(gh pr list --search $(git rev-parse $REF) --state=all --json "number,url,reviewDecision,reviews,author,state,baseRefName,closedAt,createdAt,mergedAt,assignees,reviews,reviewDecision,reviewRequests,labels")
 
-export PR=$(gh api graphql -F repoOwner='{owner}' -F repoName='{repo}' -F sha="f7721f0" -f query="$(cat ../gha-reusable-workflows/vsa/pr-query.graphql)" --jq '.data.repository.commit')
+export PR=$(gh api graphql -F repoOwner="$OWNER" -F repoName="$REPO" -F sha="$SHA" --jq '.data.repository.commit' -f query='
+query ($repoOwner: String!, $repoName: String!, $sha: String!) {
+  repository(name: $repoName, owner: $repoOwner) {
+    commit: object(expression: $sha) {
+      ... on Commit {
+        commitUrl
+        associatedPullRequests(first: 1) {
+          nodes {
+            author {
+              login
+              url
+            }
+            assignees(first: 10) {
+              nodes {
+                login
+                url
+              }
+            }
+            baseRefOid
+            headRefName
+            headRefOid
+            merged
+            number
+            reviewDecision
+            state
+            title
+            number
+            body
+            state
+            createdAt
+            mergedAt
+            closedAt
+          }
+        }
+      }
+    }
+  }
+}')
 
 cat | jq . <<EOF
 {
   "pull_requests": $PR,
   "repository": {
-    "name": "$REPO",
+    "name": "$name/$REPO",
     "ref": "$REF",
     "digest": {"sha1": "$SHA"}
   }

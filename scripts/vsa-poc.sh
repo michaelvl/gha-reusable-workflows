f00bb63ff112ec9cd5bb909d2bbefa9824a849fc
michaelvl <mvl.gh@network42.dk>
2024-02-10 10:16:35 +0100

    Update VSA to intoto 1.0

--- scripts/vsa-poc.sh
+++ scripts/vsa-poc.sh
@@ -7,16 +7,19 @@ POLICY_DIGEST=$3
 # This is also RFC3339 format - OPA understands this format
 NOW=$(date --iso-8601=seconds -u)
 
+# https://slsa.dev/spec/v1.0/verification_summary
 cat <<EOF
 {
   "verifier": {
     "id": "$POLICY"
   },
-  "verifiedAt": "$NOW",
+  "timeVerified": "$NOW",
   "policy": {
     "uri": "$POLICY",
     "digest": {"sha1": "$POLICY_DIGEST"}
   },
-  "verificationResult": "$STATUS"
+  "verificationResult": "$STATUS",
+  "verifiedLevels": ["SLSA_BUILD_LEVEL_3"],
+  "slsaVersion": "1.0"
 }
 EOF
